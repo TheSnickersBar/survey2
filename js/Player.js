f0bb53ec510f27f3e57d2124959306b62d0020ea
TheSnickersBar <39471794+TheSnickersBar@users.noreply.github.com>
2020-07-10 10:27:37 -0400

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,33 @@
+class Player{
+    constructor()
+    {
+      this.name = null;
+      this.q1 = null;
+      this.q2 = null;
+      this.q3 = null;
+      
+
+    }
+    display(){
+      
+    }
+
+   
+
+    update(){
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).update({
+          emailid: this.name,
+          question1: this.q1,
+          question2: this.q2,
+          question3: this.q3
+    
+        });
+      }
+
+      
+  
+    }
+
+    
+    
